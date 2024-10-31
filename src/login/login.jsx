ff4ef23cc61cb0ed7301ff55565001f838a9b939
adraney0 <adraney0@byu.edu>
2024-10-31 10:56:44 -0600

    convert login to component

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main className = "container-fluid bg-second text-center">
+        <h2>Welcome to Triangular Peg Solitaire</h2>
+        <form method = "get" action = "play.html">
+            <div className = "input-group mb-3">
+                <span className = "input-group-text">Email</span>
+                <input className = "form-control" type = "email" placeholder = "your@email.com" />
+            </div>
+            <div class = "input-group mb-3">
+                <span className = "input-group-text">Password</span>
+                <input className = "form-control" type = "password" placeholder = "password" />
+            </div>
+            <button type = "submit" className = "btn btn-main">Sign In</button>
+            <button type = "submit" className = "btn btn-second">Create Account</button>
+        </form>
+        <hr />
     </main>
   );
 }
