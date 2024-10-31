a5b8f4a1d7eceeca4fbcc3e8dbce008c833dc576
adraney0 <adraney0@byu.edu>
2024-10-31 10:47:12 -0600

    convert scores to component

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,48 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
     return (
-        <div className='body bg-main text-dark'>
-        <header className = "container fluid">
-            <nav className = "navbar fixed-top navbar-dark">
-                <a className = "navbar-brand" href = "#">Triangular Peg Solitaire</a>
-                <menu class = "navbar-nav">
-                    <li className = "nav-item"><a className = "nav-link active"href = "index.html">Login</a></li>
-                    <li className = "nav-item"><a className = "nav-link" href = "play.html">Play</a></li>
-                    <li className = "nav-item"><a className = "nav-link" href = "scores.html">High Scores</a></li>
-                    <li className = "nav-item"><a className = "nav-link" href = "about.html">About</a></li>
-                </menu>
-            </nav>
-        </header>
-  
-        <main>App components go here</main>
+        <BrowserRouter>
+            <div className='body bg-main text-dark'>
+                <header className = "container fluid">
+                    <nav className = "navbar fixed-top navbar-dark">
+                        <div className = "navbar-brand" href = "#">Triangular Peg Solitaire</div>
+                        <menu class = "navbar-nav">
+                            <li className = "nav-item"><NavLink className = 'nav-link' to = ''>Login</NavLink></li>
+                            <li className = "nav-item"><NavLink className = 'nav-link' to = 'play'>Play</NavLink></li>
+                            <li className = "nav-item"><NavLink className = 'nav-link' to = 'scores'>High Scores</NavLink></li>
+                            <li className = "nav-item"><NavLink className = 'nav-link' to = 'about'>About</NavLink></li>
+                        </menu>
+                    </nav>
+                </header>
+        
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/play' element={<Play />} />
+                    <Route path='/scores' element={<Scores />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
 
-        <footer class = "bg-main text-white">
-            <div class = "container-fluid">
-                <span>Belle Draney</span>
-                <a href = "https://github.com/adraney0/CS260startup/tree/main" class = "custom-link">GitHub</a>
+                <footer class = "bg-main text-white">
+                    <div class = "container-fluid">
+                        <span>Belle Draney</span>
+                        <a href = "https://github.com/adraney0/CS260startup/tree/main" class = "custom-link">GitHub</a>
+                    </div>
+                    
+                </footer>
             </div>
-            
-        </footer>
-
-      </div>
+        </BrowserRouter>
     );
+  }
+
+  function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
   }
--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
+import "./score.css";
+
 export function Scores() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>scores displayed here</div>
+    <main className = "container-fluid bg-second text-center">
+            <table className = "table table-warning">
+                <thead className = "table-dark">
+                    <tr>
+                        <th className = "btn-main">#</th>
+                        <th className = "btn-main">Name</th>
+                        <th className = "btn-main">Pegs Remaining</th>
+                        <th className = "btn-main">Moves</th>
+                        <th className = "btn-main">Date</th>
+                    </tr>
+                </thead>
+                <tbody className = "table-primary">
+                    <tr>
+                        <td>1</td>
+                        <td>John Smith</td>
+                        <td>1</td>
+                        <td>14</td>
+                        <td>9.26.24</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Sally Smith</td>
+                        <td>2</td>
+                        <td>15</td>
+                        <td>9.25.24</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Bob Smith</td>
+                        <td>2</td>
+                        <td>16</td>
+                        <td>9.27.24</td>
+                    </tr>
+                </tbody>
+            </table>
+        <hr />
     </main>
   );
 }
